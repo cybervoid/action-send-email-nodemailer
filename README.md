28c56b45f89e2ef1252fa984d2d9a1ce9aa3a4a2
Rafael Gil <rafaelgil83@gmail.com>
2022-03-25 23:24:03 -0400

    added github action

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # action-send-email-nodemailer
 
+This action allows to Send an Email via Nodemailer.
 
+## Inputs
+
+## `to`
+
+**Required** The Receiver
+
+## `from`
+
+**Required** The Sender
+
+## `subject`
+
+**Required** Email subject
+
+## `body`
+
+**Required** Email body
+
+## Outputs
+
+## `time`
+
+The time we greeted you.
+
+## Example usage
+
+uses: actions/hello-world-javascript-action@v1.1 with:
+who-to-greet: 'Mona the Octocat'
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 "use strict";
 
+const core = require('@actions/core');
+const github = require('@actions/github');
 let nodemailer = require("nodemailer");
 let aws = require("@aws-sdk/client-ses");
 
 async function main() {
+
+    // from: process.env.FROM,
+    //     to: process.env.TO,
+    //     subject: process.env.SUBJECT,
+    //     text: process.env.BODY,
+    //     html: process.env.BODY,
+
+    const to = core.getInput('to');
+    const from = core.getInput('from');
+    const subject = core.getInput('subject');
+    const body = core.getInput('body');
+
     const ses = new aws.SES({
         apiVersion: "2010-12-01",
         region: "us-east-1",
@@ -13,12 +27,12 @@ async function main() {
     });
 
     // send mail with defined transport object
-    let info = await transporter.sendMail({
-        from: process.env.FROM,
-        to: process.env.TO,
-        subject: process.env.SUBJECT,
-        text: process.env.BODY,
-        html: process.env.BODY,
+    const info = await transporter.sendMail({
+        from: from,
+        to: to,
+        subject: subject,
+        text: body,
+        html: body,
     });
 
     console.log("Message sent: %s", info.messageId);
@@ -28,4 +42,4 @@ async function main() {
     // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-main().catch(console.error);
+main().catch(err => core.setFailed(err));
